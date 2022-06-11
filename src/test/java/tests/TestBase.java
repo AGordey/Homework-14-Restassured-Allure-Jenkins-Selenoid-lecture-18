7707c3daf24a10a07b6ba2d8c11de56d19312b69
AGordey <gordey.andrej@gmail.com>
2022-06-11 23:02:59 +0300

    Init

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    public static final String getSingleResourseAPI = "/unknown/2";
+    public static final String postRegisterSuccessfulAPI = "/register";
+    public static final String putUpdateApi = "/users/2";
+    public static final String patchUpdateApi = "/users/2";
+    public static final String deleteApi = "/users/2";
+
+    static String login = "qaguru@qa.guru",
+            password = "qaguru@qa.guru1",
+            authCookieName = "NOPCOMMERCE.AUTH";
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+    }
+}
